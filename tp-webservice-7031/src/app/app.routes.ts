9d529dde7915d58b3a2ddd89c3ce9b050cc58ea1
orellanaariana <cococonpitula@gmail.com>
2026-05-24 11:33:15 -0300

    ArianaOrellana

--- tp-webservice-7031/src/app/app.routes.ts
+++ tp-webservice-7031/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 import { Home } from './components/layout/home/home.component'; 
 import { PeliculasComponent } from './components/punto-a/peliculas.component';
 import { AutosComponent } from './components/punto-b/autos.component'; // Reemplaza por tu componente real
+import { ConversorComponent } from './components/punto-c/conversor.component'; // Reemplaza por tu componente real
 // Importa aquí los demás (Conversor, TextToSpeech, etc.)
 
 export const routes: Routes = [
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'app-peliculas', component: PeliculasComponent },
   { path: 'app-autos', component: AutosComponent }, // Reemplaza por tu componente real
+  { path: 'app-conversor', component: ConversorComponent }, // Reemplaza por tu componente real
   { path: 'parte2', component: Home }, // Reemplaza por tu componente real
   { path: 'punto3', component: Home }, // Reemplaza por tu componente real
   
