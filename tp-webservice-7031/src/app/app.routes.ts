193652259556e41278ba57ee41bb8b95850f290a
orellanaariana <cococonpitula@gmail.com>
2026-05-24 17:48:48 -0300

    ArianaOrellana

--- tp-webservice-7031/src/app/app.routes.ts
+++ tp-webservice-7031/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Home } from './components/layout/home/home.component';
 import { PeliculasComponent } from './components/punto-a/peliculas.component';
 import { AutosComponent } from './components/punto-b/autos.component'; // Reemplaza por tu componente real
 import { ConversorComponent } from './components/punto-c/conversor.component'; // Reemplaza por tu componente real
+import { TtsComponent } from './components/punto-d/tts.component'; // Reemplaza por tu componente real
 // Importa aquí los demás (Conversor, TextToSpeech, etc.)
 
 export const routes: Routes = [
@@ -13,9 +14,8 @@ export const routes: Routes = [
   { path: 'app-peliculas', component: PeliculasComponent },
   { path: 'app-autos', component: AutosComponent }, // Reemplaza por tu componente real
   { path: 'app-conversor', component: ConversorComponent }, // Reemplaza por tu componente real
-  { path: 'parte2', component: Home }, // Reemplaza por tu componente real
-  { path: 'punto3', component: Home }, // Reemplaza por tu componente real
-  
+  { path: 'app-tts', component: TtsComponent }, // Reemplaza por tu componente real
+  // Agrega aquí las rutas para tus otros componentes (Autos, Conversor, TextToSpeech, etc.)
   // 3. Rutas por defecto y comodines (¡Muy recomendado!)
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Si la ruta está vacía, va a 'home'
   { path: '**', redirectTo: 'home' } // Si el usuario escribe una URL que no existe, vuelve a 'home'
--- tp-webservice-7031/src/app/components/punto-d/punto-d.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-punto-d',
-  imports: [],
-  templateUrl: './punto-d.html',
-  styleUrl: './punto-d.css',
-})
-export class PuntoD {}
